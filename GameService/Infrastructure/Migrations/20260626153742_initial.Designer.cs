8abc3c9c2c685da4a58becfe88f64cb53f82cfb8
Long Dong <longdong32120@gmail.com>
2026-06-26 22:37:55 +0700

    update

--- GameService/Infrastructure/Migrations/20260626153742_initial.Designer.cs
+++ GameService/Infrastructure/Migrations/20260626153742_initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(RelationalDB))]
-    [Migration("20260626152912_initial")]
+    [Migration("20260626153742_initial")]
     partial class initial
     {
         /// <inheritdoc />
