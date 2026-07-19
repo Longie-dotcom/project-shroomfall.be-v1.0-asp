f3ee29cafc0ed2cd11f495b0866e57d515d21347
Long Dong <longdong32120@gmail.com>
2026-07-19 18:02:15 +0700

    fix: change method name

--- GameService/Infrastructure/Realtime/AdminHub.cs
+++ GameService/Infrastructure/Realtime/AdminHub.cs
@@ -19,13 +19,21 @@ namespace Infrastructure.Realtime
         #region Methods
         public override async Task OnConnectedAsync()
         {
+            Console.WriteLine($"[SignalR] AdminHub: Client Connected. ConnectionID: {Context.ConnectionId}");
+
             await Groups.AddToGroupAsync(Context.ConnectionId, Constraint.ADMIN_REALTIME_GROUP);
+
+            Console.WriteLine($"[SignalR] AdminHub: Added {Context.ConnectionId} to group {Constraint.ADMIN_REALTIME_GROUP}");
+
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
+            Console.WriteLine($"[SignalR] AdminHub: Client Disconnected. ConnectionID: {Context.ConnectionId}");
+
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, Constraint.ADMIN_REALTIME_GROUP);
+
             await base.OnDisconnectedAsync(exception);
         }
         #endregion
