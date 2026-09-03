c764ec4159724773af46387a424c5ad842b9cbc0
Long Dong <longdong32120@gmail.com>
2026-09-03 23:48:49 +0700

    add: debug

--- GameService/Infrastructure/Messaging/Consumer/GameStartupConsumer.cs
+++ GameService/Infrastructure/Messaging/Consumer/GameStartupConsumer.cs
@@ -32,6 +32,8 @@ namespace Infrastructure.Messaging.Consumer
         public async Task Consume(
             ConsumeContext<GameStartupDTO> context)
         {
+            Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
             var message = context.Message;
 
             // LOAD CACHE
