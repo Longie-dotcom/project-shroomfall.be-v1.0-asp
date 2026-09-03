a986fc94d377b92938251a3e5377be247b6fae90
Long Dong <longdong32120@gmail.com>
2026-09-03 21:15:01 +0700

    add: debug

--- GameService/Infrastructure/Messaging/Consumer/GameStartupConsumer.cs
+++ GameService/Infrastructure/Messaging/Consumer/GameStartupConsumer.cs
@@ -40,7 +40,6 @@ namespace Infrastructure.Messaging.Consumer
             // BOOT WORLD
             await bootstrapService.LoadAsync();
             Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-            logger.LogInformation("World bootup successfully! Game started!");
         }
         #endregion
     }
